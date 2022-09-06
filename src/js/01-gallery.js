f1ad2467634b4652309cb61591750b472ed7903e
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-09-06 21:07:14 +0300

    done 2,1 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,64 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
 const galeryDiv = document.querySelector('.gallery');
-
-function createItemsMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    /></a></div>`;
-    })
-    .join('');
-}
-
-galeryDiv.addEventListener('click', onDivImagesClick);
 
 const itemsMarkup = createItemsMarkup(galleryItems);
 
 galeryDiv.innerHTML = itemsMarkup;
 
-function onDivImagesClick(evt) {
-  const isImage = evt.target.classList.contains('gallery__image');
-
-  if (!isImage) {
-    return;
-  }
-
-  const instance = basicLightbox.create(
-    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
-    {
-      onShow: instance => {
-        galeryDiv.addEventListener('keydown', closePicture);
-      },
-
-      onClose: instance => {
-        galeryDiv.removeEventListener('keydown', closePicture);
-      },
-    }
-  );
-
-  instance.show();
-
-  // Заборона відкриття по посиланню
-  blockAction(evt);
+function createItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
 }
 
-function closePicture(instance) {
-  if (evt.code === 'Escape') {
-    instance.close();
-  }
-}
-
-function blockAction(evt) {
-  evt.preventDefault();
-}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
